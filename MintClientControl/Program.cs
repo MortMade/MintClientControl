3602ab80cc7b16f47e148bb1e825c28824535f9f
Morten Lyngberg Pedersen <morten.lyngberg@gmail.com>
2022-05-16 10:39:27 +0200

    Initial mvvm test

--- MintClientControl/Program.cs
+++ MintClientControl/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MintClientControl.Models;
+using MintClientControl.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -18,6 +20,8 @@ namespace MintClientControl
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddTransient<IFetchDataModel, FetchDataModel>();
+            builder.Services.AddTransient<IFetchDataViewModel, FetchDataViewModel>();
 
             await builder.Build().RunAsync();
         }
---
+++ MintClientControl/Views/FetchDataView.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using MintClientControl.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MintClientControl.Views
+{
+    public class FetchDataBase : ComponentBase
+    {
+        [Inject]
+        public IFetchDataViewModel ViewModel { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await ViewModel.RetrieveForecastsAsync();
+        }
+    }
+}
