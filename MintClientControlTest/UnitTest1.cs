79da5493d3f15f0b63a995e7248fc09deda1773d
Morten Lyngberg Pedersen <morten.lyngberg@gmail.com>
2022-05-31 23:30:09 +0200

    Added a bUnit test project

---
+++ MintClientControlTest/UnitTest1.cs
@@ -0,0 +1,39 @@
+using Bunit;
+using MintClientControl.Pages;
+using System;
+using Xunit;
+
+namespace MintClientControlTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CounterShouldIncrementWhenClicked()
+        {
+            // Arrange: render the Counter.razor component
+            using var ctx = new TestContext();
+            var cut = ctx.RenderComponent<Counter>();
+
+            // Act: find and click the <button> element to increment
+            // the counter in the <p> element
+            cut.Find("button").Click();
+
+            // Assert: first find the <p> element, then verify its content
+            cut.Find("p").MarkupMatches("<p>Current count: 1</p>");
+        }
+        [Fact]
+        public void AddItemTest()
+        {
+            // Arrange: render the Counter.razor component
+            using var ctx = new TestContext();
+            var cut = ctx.RenderComponent<Counter>();
+
+            // Act: find and click the <button> element to increment
+            // the counter in the <p> element
+            cut.Find("button").Click();
+
+            // Assert: first find the <p> element, then verify its content
+            cut.Find("p").MarkupMatches("<p>Current count: 1</p>");
+        }
+    }
+}
